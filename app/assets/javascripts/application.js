020e1ae88f9140bd99556fc4c12d081fcb2e3e11
lwangenheim <lwangenheim@gmail.com>
2014-05-01 21:53:53 -0400

    added some stuff

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,14 @@
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// about supported directives.
+// compiled file.
+// listed below.
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//= require angular
+//= require angular-resource
+//= require main.js
+//= require_tree .
+//= require_tree .
+//= require_tree ./angular
